a3761b57d427572a2b64d3bfffce657104add83d
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-06-07 12:36:43 -0500

    add Car class logic and test it

--- ooexercises.test.js
+++ ooexercises.test.js
@@ -5,3 +5,12 @@ describe("Vehicle Test", () => {
         expect(firstVehicle.toString()).toEqual("The vehicle is a Volkswagen Tiguan from 2022.");
     });
 });
+
+describe("Car Test", () => {
+    it("should test to see that vehicle was created, if it inherits everything and has 4 wheels", () => {
+        let firstCar = new Car("Volkswagen", "Tiguan", "2022");
+        expect(firstCar.honk()).toEqual("Beep.");
+        expect(firstCar.toString()).toEqual("The vehicle is a Volkswagen Tiguan from 2022.");
+        expect(firstCar.numWheels()).toEqual(4);
+    });
+});
